b9e046fe5d9963813677626ba6c030e59f77b055
Bernardo Piccoli <bernardokpiccoli@gmail.com>
2025-09-03 20:23:36 -0300

    style: adicionado lib tailwind para estilizacao de componentes

--- App.js
+++ App.js
@@ -1,61 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import { useState } from "react";
+import "./global.css";
+import { Text, View } from "react-native";
 
 export default function App() {
-  const [count, setCount] = useState(null);
-  const handlePress = () => {
-    setCount(count + 1);
-  };
-
-  const handleClearCounter = () => {
-    setCount(0);
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.txt}>Contagem: {count}</Text>
-      <View style={styles.buttons}>
-        <TouchableOpacity style={styles.press} onPress={handlePress}>
-          <Text>Press</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.clear} onPress={handleClearCounter}>
-          <Text>Clear</Text>
-        </TouchableOpacity>
-      </View>
-
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  txt: {
-    color: "green",
-  },
-  buttons: {
-    display: "flex",
-    flexDirection: "row",
-    width: 100,
-    justifyContent: "space-between",
-    margin: 10,
-  },
-  press: {
-    padding: 5,
-    borderRadius: 5,
-    margin: 5,
-    backgroundColor: "#ffc2b8",
-  },
-  clear: {
-    padding: 5,
-    borderRadius: 5,
-    margin: 5,
-    backgroundColor: "#ffc2b8",
-  },
-});
