bc95b0b2fbbd03acda84d53fba87b76a25284807
Bernardo Piccoli <bernardo@redrive.com.br>
2025-09-08 18:38:54 -0300

    style: adicionando atributos para o dark mode

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 // @ts-ignore
 import "./global.css";
 import { useState } from "react";
-import { Text, View, Alert } from "react-native";
+import { Text, View, Alert, useColorScheme } from "react-native";
 import EmailInput from "@components/inputs/email";
-import PasswordInput from "@components/inputs/password";
+import { PasswordInput } from "@components/inputs/password";
 import Button from "@components/buttons/button";
 import LogoTaskly from "@components/logo";
+
 export default function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const handleLogin = () => {
     if (!email || !password) {
       Alert.alert("Erro", "Preencha todos os campos.");
@@ -17,16 +17,46 @@ export default function App() {
     }
     Alert.alert("Login", `Bem-vindo, ${email}!`);
   };
+  const theme = useColorScheme();
+  console.log(theme);
 
   return (
-    <View className="flex-1 justify-center items-center bg-white px-6">
+    <View
+      className={`${
+        theme === "dark" ? "bg-gray-900" : "bg-white"
+      } flex-1 justify-center items-center px-6`}
+    >
       <LogoTaskly />
-      <Text className="text-2xl font-bold mb-8" style={{ color: "#233A6A" }}>
-        camila te amo S2
+      <Text
+        className={`${
+          theme === "dark" ? "color-white" : " color-[#233A6A]"
+        } text-2xl font-bold mb-8`}
+      >
+        Taskly
       </Text>
       <EmailInput email={email} setEmail={setEmail} />
       <PasswordInput password={password} setPassword={setPassword} />
       <Button onPress={handleLogin} title="Entrar" />
+
+      <View className="flex direction-row gap-4 mt-4 w-10/12 items-center">
+        <Text
+          className={`${theme === "dark" ? "text-white" : "text-[#233A6A]"}`}
+        >
+          Esqueceu sua senha?
+        </Text>
+        <Text
+          className={`${theme === "dark" ? "text-white" : "text-[#233A6A]"}  `}
+        >
+          Não possuí conta ainda?{" "}
+          <Text
+            className={`${
+              theme === "dark" ? "text-white" : "text-[#233A6A]"
+            } underline`}
+          >
+            Cadastre-se!
+          </Text>
+        </Text>
+      </View>
     </View>
   );
 }
--- src/components/inputs/password.tsx
+++ src/components/inputs/password.tsx
@@ -1,11 +1,18 @@
-import { TextInput } from "react-native";
-export default function PasswordInput({
-  password,
-  setPassword,
-}: {
+import type React from "react";
+import { TextInput, Keyboard } from "react-native";
+interface PasswordInputProps {
   password: string;
   setPassword: (password: string) => void;
-}) {
+  isKeyboardVisible?: boolean;
+  setIsKeyboardVisible?: (isKeyboardVisible: boolean) => boolean;
+}
+
+const PasswordInput: React.FC<PasswordInputProps> = ({
+  isKeyboardVisible,
+  setIsKeyboardVisible,
+  password,
+  setPassword,
+}) => {
   return (
     <TextInput
       className="w-full h-12 px-4 mb-4 border rounded-lg text-base bg-[#F3F6FF] color-[#233A6A] border-[#233A6A]"
@@ -13,7 +20,9 @@ export default function PasswordInput({
       placeholderTextColor="#233A6A"
       value={password}
       onChangeText={setPassword}
+      onSubmitEditing={Keyboard.dismiss}
       secureTextEntry
     />
   );
-}
+};
+export { PasswordInput };
